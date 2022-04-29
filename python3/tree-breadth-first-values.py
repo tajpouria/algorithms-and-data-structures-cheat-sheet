7724fbb82329692bf20f9ed6df2873a2afee7a27
tajpouria <tajpouria.dev@gmail.com>
2022-04-29 20:21:27 +0400

    wip

---
+++ python3/tree-breadth-first-values.py
@@ -0,0 +1,86 @@
+class None:
+    left = None 
+    right = None
+    __init__(self, data):
+        self.data = data 
+
+def
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   #   b     c
+#  / \     # d   e     f
+
+breadth_first_values(a) 
+#    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   #   b     c
+#  / \     # d   e     f
+#    /       #   g         h
+
+breadth_first_values(a) 
+#   -> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+a = Node('a')
+
+#      a
+
+breadth_first_values(a) 
+#    -> ['a']
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+x = Node('x')
+
+a.right = b
+b.left = c
+c.left = x
+c.right = d
+d.right = e
+
+#      a
+#       #        b
+#       /
+#      c
+#    /  #   x    d
+#         #          e
+
+breadth_first_values(a) 
+#    -> ['a', 'b', 'c', 'x', 'd', 'e']
+
+breadth_first_values(None) 
+#    -> []
